ce5e8242f96d14dc4449c64050640a882346e1ab
wahaj-47 <wahajhssn@gmail.com>
2019-12-09 11:16:43 +0500

    Project Closed

--- routes/users.js
+++ routes/users.js
@@ -48,6 +48,28 @@ router.post("/delete", verifyToken, function(req, res, next) {
 	});
 });
 
+router.post("/deleteUser", verifyToken, function(req, res, next) {
+	jwt.verify(req.token, "secretkey", (err, authData) => {
+		if (err) {
+			res.send({ error: err });
+		} else {
+			connection.query(
+				`Delete from users where email="${req.body.email}"`,
+				function(err, results, fields) {
+					if (err) {
+						res.send({
+							error: err.code,
+							msg: err.sqlMessage
+						});
+					} else {
+						res.send({ success: true, results });
+					}
+				}
+			);
+		}
+	});
+});
+
 function verifyToken(req, res, next) {
 	const bearerHeader = req.headers["authorization"];
 	if (typeof bearerHeader !== "undefined") {
